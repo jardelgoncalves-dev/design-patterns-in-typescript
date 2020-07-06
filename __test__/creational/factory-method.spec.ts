c00c447d38ae86e3bb7e129e63d6965ea038cbc2
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-07-06 01:03:20 -0300

    test: add test for factory method

---
+++ __test__/creational/factory-method.spec.ts
@@ -0,0 +1,17 @@
+import { Creator } from '../../src/creational/factory-method';
+
+describe('Creational/Factory Method Test', () => {
+  it('should create an football tournaments', () => {
+    const football = Creator.createFootbalTournament('UEFA Champions League');
+
+    expect(football).toBeDefined();
+    expect(football.name).toBe('UEFA Champions League');
+    expect(football.numberTeams).toBe(79);
+  });
+
+  it('should return null if football tournament does not exist', () => {
+    const football = Creator.createFootbalTournament('Liga BBC');
+
+    expect(football).toBeNull();
+  });
+});
