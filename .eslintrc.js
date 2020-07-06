8d8f97318cb15d6836fef34b6127ee0541e96751
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-07-06 01:21:16 -0300

    refactor: disable explicit-function-return-type and interf-name-pefix

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,8 @@ module.exports = {
     },
   },
   extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
-  rules: {},
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+  },
 };
